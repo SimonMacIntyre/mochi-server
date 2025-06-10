78b428fca5f2da2c9793dfc823071b37aeda26a2
Simon <simonpmacintyre@gmail.com>
2025-06-10 07:08:42 -0300

    do not check qos

--- server.go
+++ server.go
@@ -913,7 +913,7 @@ func (s *Server) processPublish(cl *Client, pk packets.Packet) error {
 	pkx, err := s.hooks.OnPublish(cl, pk)
 	if err == nil {
 		pk = pkx
-	} else if errors.Is(err, packets.ErrRejectPacket) && (cl.Properties.ProtocolVersion != 5 || pk.FixedHeader.Qos == 0) {
+	} else if errors.Is(err, packets.ErrRejectPacket) && cl.Properties.ProtocolVersion != 5 {
 		return nil
 	} else if errors.Is(err, packets.CodeSuccessIgnore) {
 		pk.Ignore = true
